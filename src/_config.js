60198de3cb30e4af0e25f02c390f809a095d9e67
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-04-14 10:49:23 -0600

    updated config again

--- src/_config.js
+++ src/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   development: 'mongodb://localhost/first-mean-app',
   test: 'mongodb://localhost/first-mean-app-testing',
-  production: process.env.mongoURI
+  production: process.env.MONGODB_URI
 };
 
 module.exports = config;
